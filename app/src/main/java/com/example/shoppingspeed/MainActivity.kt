553f83878e6252ef183028ba0e3b67c87d91f4e9
sudeepbiswa <sudeepbiswa@gmail.com>
2023-03-27 23:05:22 -0500

    Working on Main Menu

--- app/src/main/java/com/example/shoppingspeed/MainActivity.kt
+++ app/src/main/java/com/example/shoppingspeed/MainActivity.kt
@@ -3,41 +3,38 @@ package com.example.shoppingspeed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.shoppingspeed.ui.theme.ShoppingSpeedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ShoppingSpeedTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Image(painter = painterResource(id = R.drawable.backgroundimage), contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier.fillMaxSize())
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.background(Color.Transparent)
+                .fillMaxSize()) {
+                Text("Speed", fontSize = 40.sp)
+                Text("Shopper", fontSize = 30.sp)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShoppingSpeedTheme {
-        Greeting("Android")
-    }
-}
