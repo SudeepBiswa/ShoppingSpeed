601bc41bec5f6e9976bbae247392add27a227ad7
sudeepbiswa <sudeepbiswa@gmail.com>
2023-03-27 23:26:16 -0500

    Working on the Main Menu 2

--- app/src/main/java/com/example/shoppingspeed/MainActivity.kt
+++ app/src/main/java/com/example/shoppingspeed/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,10 +19,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shoppingspeed.ui.theme.ShoppingSpeedTheme
+import com.example.shoppingspeed.ui.theme.bubbley
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +33,22 @@ class MainActivity : ComponentActivity() {
             Image(painter = painterResource(id = R.drawable.backgroundimage), contentDescription = null,
             contentScale = ContentScale.FillHeight,
             modifier = Modifier.fillMaxSize())
+
             Column(horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.background(Color.Transparent)
-                .fillMaxSize()) {
-                Text("Speed", fontSize = 40.sp)
-                Text("Shopper", fontSize = 30.sp)
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxSize()
+                .padding(top = 15.dp)) {
+                Text("Speed", fontSize = 90.sp, fontFamily = bubbley, color = Color.Red)
+                Text("Shopper", fontSize = 50.sp, fontFamily = bubbley, color = Color.Red)
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier.fillMaxSize()
+                .padding(bottom = 40.dp)) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Start", fontSize = 60.sp, fontFamily = bubbley)
+                }
             }
         }
     }
--- app/src/main/java/com/example/shoppingspeed/ui/theme/Type.kt
+++ app/src/main/java/com/example/shoppingspeed/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.shoppingspeed.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.shoppingspeed.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +28,5 @@ val Typography = Typography(
     )
     */
 )
+
+val bubbley = FontFamily(Font(R.font.bubbly))
